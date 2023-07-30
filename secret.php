57b1f9040744a436cdb69d46ff014bbb73dfdaa9
Tengku Arya Saputra <33353823+AryaSec1337@users.noreply.github.com>
2023-07-30 16:04:27 +0700

    Update secret.php

--- secret.php
+++ secret.php
@@ -4,8 +4,7 @@ if (!isset($_GET['url'])) {
     die(highlight_file(__FILE__));
 }
 
-$url = $_GET['url'];
-
+if($url = $_GET['url'];)}
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -16,5 +15,5 @@ $response = curl_exec($ch);
 curl_close($ch);
 
 echo $response;
-
-?> 1
+    }
+?>
